e9b7d650db9a32e21febe4daa89cb2102e0fe234
Alimam Muhamed Sesay <sesayluckey@gmail.com>
2019-06-21 16:49:18 +0200

    Added static files serve

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { Logger } from '@nestjs/common';
 
 import { AppModule } from './app.module';
@@ -7,7 +8,10 @@ import { AppModule } from './app.module';
 const port = process.env.PORT || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
+
   // app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: [
@@ -15,6 +19,7 @@ async function bootstrap() {
       'http://192.168.1.4:5000' // angular local prod
     ],
   });
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   await app.listen(port);
   Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap');
 
